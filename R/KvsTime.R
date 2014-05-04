e45e5894e66b84d80069cad8098fe24cdf7d05a5
John Baumgartner <johnbaums@gmail.com>
2014-05-04 19:40:45 +1000

    KvsTime updated to include 0 on the y axis for all populations.

--- R/KvsTime.R
+++ R/KvsTime.R
@@ -5,11 +5,12 @@
 #' @param meta The R object holding population info returned by \link{mpmeta}.
 #' @param kch The R object holding kch data returned by \link{mpkch}.
 #' @param filename A character string giving the filename (with extension ".pdf") to save the plot to.
+#' @param samelims (logical) If \code{TRUE}, the y-axis limits will be constant across plots.
 #' @param layout The number of columns and row of plots (default = 5 columns, 10 rows).
 #' @param ... Additional arguments to \link{xyplot.zoo}.
 #' @return A pdf file with a plot for each population showing carrying capacity over time.
 #' @export
-KvsTime <- function(meta, kch, filename, layout=c(5, 10), ...) {
+KvsTime <- function(meta, kch, filename, samelims=FALSE, layout=c(5, 10), ...) {
   require(zoo)
   require(lattice)
   if (!grepl('\\.pdf$', filename)) filename <- paste0(filename, '.pdf')
@@ -24,6 +25,8 @@ KvsTime <- function(meta, kch, filename, layout=c(5, 10), ...) {
   pdf(filename, paper='a4', width=8, height=11)
   print(xyplot(zoo(kch), layout=layout, 
                scales=list(alternating=FALSE, tck=c(1, 0), cex=0.8, rot=0),
-               ylab='Carrying capacity', strip=Strip, col=1, ...))
+               ylab='Carrying capacity', strip=Strip, col=1, 
+               ylim=mapply(c, 0, apply(kch, 2, max), SIMPLIFY=FALSE), 
+               ...))
   dev.off()
 }
