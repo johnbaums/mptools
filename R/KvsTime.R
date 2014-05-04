a47c0a2a2edbc3b30d778177654577ceef754279
John Baumgartner <johnbaums@gmail.com>
2014-05-04 20:13:49 +1000

    More changes to KvsTime (now plots N as well if requested)

--- R/KvsTime.R
+++ R/KvsTime.R
@@ -6,11 +6,13 @@
 #' @param kch The R object holding kch data returned by \link{mpkch}.
 #' @param filename A character string giving the filename (with extension ".pdf") to save the plot to.
 #' @param samelims (logical) If \code{TRUE}, the y-axis limits will be constant across plots.
+#' @param plotN (logical) If \code{TRUE}, mean population abundance will be plotted (solid lines) in addition to carrying capacity (dashed lines).
+#' @param results (required only if \code{plotN} is \code{TRUE}) The R object holding simulation results returned by \link{mpresults}. 
 #' @param layout The number of columns and row of plots (default = 5 columns, 10 rows).
 #' @param ... Additional arguments to \link{xyplot.zoo}.
 #' @return A pdf file with a plot for each population showing carrying capacity over time.
 #' @export
-KvsTime <- function(meta, kch, filename, samelims=FALSE, layout=c(5, 10), ...) {
+KvsTime <- function(meta, kch, filename, samelims=FALSE, plotN=FALSE, results, layout=c(5, 10), ...) {
   require(zoo)
   require(lattice)
   if (!grepl('\\.pdf$', filename)) filename <- paste0(filename, '.pdf')
@@ -29,7 +31,18 @@ KvsTime <- function(meta, kch, filename, samelims=FALSE, layout=c(5, 10), ...) {
   } else {
     scl$y <- list(limits=mapply(c, 0, apply(kch, 2, max), SIMPLIFY=FALSE))
   }
-  print(xyplot(zoo(kch), layout=layout, scales=scl,
-               ylab='Carrying capacity', strip=Strip, col=1, ...))
+  if (isTRUE(plotN)) {
+    kchn <- cbind(kch, results$results[, 'mean', -1]
+                [, match(colnames(results$results[, 'mean', -1]), 
+                         colnames(kch))])
+    colnames(kchn) <- make.unique(colnames(kchn))
+    print(xyplot(zoo(kchn), screens=rep(colnames(kch), 2),
+                 lty=rep(c(2, 1), each=ncol(kch)),
+                 layout=layout, scales=scl,
+                 ylab='Carrying capacity', strip=Strip, col=1, ...))
+  } else {
+    print(xyplot(zoo(kch), layout=layout, scales=scl,
+                 ylab='Carrying capacity', strip=Strip, col=1, ...))  
+  }
   dev.off()
 }
