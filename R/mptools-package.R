f36521e68d6c0f0f9a40749cf83474d22b05d974
John Baumgartner <johnbaums@gmail.com>
2016-02-07 18:55:25 +1100

    Add documentation for ?mptools

---
+++ R/mptools-package.R
@@ -0,0 +1,8 @@
+#' mptools: Manipulate RAMAS Metapop .mp files
+#' 
+#' Extraction and presentation of metadata and simulation results from RAMAS
+#' Metapop .mp files.
+#' 
+#' @docType package
+#' @name mptools
+NULL
