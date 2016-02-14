88bed027e8447dfe83d31c55da3923531d961e3d
John Baumgartner <johnbaums@gmail.com>
2016-02-14 14:10:17 +1100

    Change knt: subset populations; remove default layout; don't write to pdf device; return lattice object.

--- R/knt.R
+++ R/knt.R
@@ -4,58 +4,87 @@
 #' 
 #' @param meta The R object holding population info returned by \link{meta}.
 #' @param kch The R object holding kch data returned by \link{kch}.
-#' @param filename A character string giving the filename (with extension
-#'   ".pdf") to save the plot to.
+#' @param pops (Optional) A character vector of population names. The
+#'   metapopulation will be subset to these populations before plotting.
 #' @param samelims (logical) If \code{TRUE}, the y-axis limits will be constant
 #'   across plots.
-#' @param plotN (logical) If \code{TRUE}, mean population abundance will be
+#' @param show_N (logical) If \code{TRUE}, mean population abundance will be
 #'   plotted (solid lines) in addition to carrying capacity (dashed lines).
 #' @param results (required only if \code{plotN} is \code{TRUE}) The R object
 #'   holding simulation results returned by \link{results}.
-#' @param layout The number of columns and row of plots (default = 5 columns, 10
-#'   rows).
-#' @param ... Additional arguments to \link{xyplot}.
-#' @return A pdf file with a plot for each population showing carrying capacity
-#'   over time.
+#' @param plot Should the resulting lattice object be plotted?
+#' @param ... Additional arguments to \link{lattice::xyplot}, e.g., \code{layout}.
+#' @return A lattice object is returned invisibly.
 #' @importFrom zoo zoo
 #' @importFrom lattice xyplot panel.rect panel.text
 #' @export
 #' @examples
 #' mp <- system.file('example.mp', package='mptools')
 #' met <- meta(mp)
-#' knt(met, kch(met, dirname(mp)), f <- tempfile(fileext='.pdf'), plotN=TRUE, 
-#'     results=results(mp))
+#' 
+#' # Subset of populations
+#' knt(met, pops=c('Pop 169', 'Pop 170', 'Pop 174', 'Pop 175'), 
+#'     kch(met, dirname(mp)), show_N=TRUE, results=results(mp),
+#'     layout=c(2, 2), samelims=TRUE)
+#'     
+#' # All populations - set layout appropriately!
+#' pdf(f <- tempfile(fileext='.pdf'))
+#' knt(met, kch(met, dirname(mp)), show_N=TRUE, results=results(mp), 
+#'     layout=c(5, 6))
+#' dev.off()
 #' file.show(f)
-knt <- function(meta, kch, filename, samelims=FALSE, plotN=FALSE, results, 
-                    layout=c(5, 10), ...) {
-  if (!grepl('\\.pdf$', filename)) filename <- paste0(filename, '.pdf')
+knt <- function(meta, kch, pops, samelims=FALSE, show_N=FALSE, results, 
+                plot=TRUE, ...) {
+  if(!missing(pops)) {
+    if(any(!pops %in% meta$popName)) {
+      warning('Some populations not in meta:\n', 
+              setdiff(pops, meta$popName),
+              call.=FALSE)
+    }
+    meta <- subset(meta, popName %in% pops)
+    kch <- kch[, intersect(pops, colnames(kch))]
+    if(any(!pops %in% colnames(kch))) {
+      warning('Some populations not in kch:\n', 
+              setdiff(pops, colnames(kch)),
+              call.=FALSE)
+    }
+  }
   hasInitN <- meta$initN > 0
   Strip <- function(which.panel, factor.levels, ...) {
     lattice::panel.rect(0, 0, 1, 1,
-               col = ifelse(hasInitN, 'lightsteelblue2', 'gray90')[which.panel],
-               border = 1)
+                        col = ifelse(hasInitN, 'lightsteelblue2', 'gray90')[which.panel],
+                        border = 1)
     lattice::panel.text(x = 0.5, y = 0.5, lab = factor.levels[which.panel],
-               font = ifelse(hasInitN, 2, 1)[which.panel])
+                        font = ifelse(hasInitN, 2, 1)[which.panel])
   }
-  pdf(filename, paper='a4', width=8, height=11)
   scl <- list(alternating=FALSE, tck=c(1, 0), cex=0.8, rot=0)
   if (isTRUE(samelims)) {
     scl$y <- list(relation = "same")
   } else {
     scl$y <- list(limits=mapply(c, 0, apply(kch, 2, max), SIMPLIFY=FALSE))
   }
-  if (isTRUE(plotN)) {
+  if (isTRUE(show_N)) {
+    if(!missing(pops)) {
+      if(any(!pops %in% dimnames(results$results)[[3]])) {
+        warning('Some populations not in results:\n', 
+                setdiff(pops, dimnames(results$results)[[3]]),
+                call.=FALSE)
+      }
+      results$results <- results$results[,, c('ALL', pops)]
+    }
     kchn <- cbind(kch, results$results[, 'mean', -1]
                 [, match(colnames(results$results[, 'mean', -1]), 
                          colnames(kch))])
     colnames(kchn) <- make.unique(colnames(kchn))
-    print(lattice::xyplot(zoo::zoo(kchn), screens=rep(colnames(kch), 2),
-                 lty=rep(c(2, 1), each=ncol(kch)),
-                 layout=layout, scales=scl,
-                 ylab='Carrying capacity', strip=Strip, col=1, ...))
+    p <- lattice::xyplot(zoo::zoo(kchn), screens=rep(colnames(kch), 2),
+                         lwd=rep(c(3, 1), each=ncol(kch)),
+                         scales=scl, ylab='Population size', 
+                         strip=Strip, col=rep(c('gray85', 1), each=ncol(kch)), 
+                         ...)
   } else {
-    print(lattice::xyplot(zoo::zoo(kch), layout=layout, scales=scl,
-                 ylab='Carrying capacity', strip=Strip, col=1, ...))  
+    p <- lattice::xyplot(zoo::zoo(kch), scales=scl, 
+                         ylab='Carrying capacity', strip=Strip, col=1, ...)
   }
-  dev.off()
+  if(isTRUE(plot)) print(p)
+  p
 }
